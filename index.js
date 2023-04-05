5dbc4aec9e56885ab2b645a953e7b21f64eca024
Sojib Mondol <mdsojibmandol@gmail.com>
2023-04-05 16:33:49 +0600

    database client change

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pr2sb9x.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wtm3mfw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -24,8 +25,9 @@ async function run(){
   try{
 
     // collection 1 
-    const usersCollection = client.db('geeksOfGurukul').collection('usersCollection');
-    const applyDataCollection = client.db('geeksOfGurukul').collection('usersApplyDataCollection');
+    const usersCollection = client.db('users').collection('usersCollection');
+    const applyDataCollection = client.db('appliedUserDetails').collection('usersApplyDataCollection');
+
 
     // user(buyer and seller) data save------------
     app.put("/users", async (req, res) => {
